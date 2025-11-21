17efe0b6feef9ea1459fd5494e65f0763d8fd256
tejdiv <tejas@divido.ai>
2025-11-21 15:27:00 -0800

    Change default port to 8080

--- habitat_llm/server/hitl_server.py
+++ habitat_llm/server/hitl_server.py
@@ -305,4 +305,4 @@ if os.path.exists("habitat_llm/server/static"):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
